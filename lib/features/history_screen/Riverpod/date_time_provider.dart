07fa35a83bc243b0e2ddc955b40e69a5ea6ce87a
Md. Rashedul Islam <santorasu09@gmail.com>
2025-08-22 10:41:46 +0600

    fixed date picker on match day

--- lib/features/history_screen/Riverpod/date_time_provider.dart
+++ lib/features/history_screen/Riverpod/date_time_provider.dart
@@ -1,3 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final selectedDateProvider = StateProvider<DateTime?>((ref) => null);
+
+// Provider for selected date
+final selectedMatchDateProvider = StateProvider<DateTime?>((ref) => null);
